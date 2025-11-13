1cd96e849ceca34a59bc7fbd5bc5e8a90a70d920
Serdar Özer <serdar.oezer@sap.com>
2025-11-13 09:01:59 +0100

    fix: unbound var release_dir fixed

--- .github/scripts/s3/run-integration-s3-compat.sh
+++ .github/scripts/s3/run-integration-s3-compat.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 # Get the directory where this script is located
 script_dir="$( cd "$(dirname "${0}")" && pwd )"
+repo_root="$(cd "${script_dir}/../../.." && pwd)"
+
 
 # Source utils from the same directory
 source "${script_dir}/utils.sh"
@@ -20,7 +22,7 @@ export BUCKET_NAME=${bucket_name}
 export S3_HOST=${s3_endpoint_host}
 export S3_PORT=${s3_endpoint_port}
 
-pushd "${release_dir}" > /dev/null
+pushd "${repo_root}" > /dev/null
   echo -e "\n running tests with $(go version)..."
   scripts/ginkgo -r --focus="S3 COMPATIBLE" s3/integration/
 popd > /dev/null
