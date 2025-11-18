7c888688195b6d13ebc94ea35f7b7ddac4f9201b
Serdar Özer <serdar.oezer@sap.com>
2025-11-18 12:08:34 +0100

    feat: alioss github workflow is added
    - scripts to run, teardown and setup are added.

---
+++ .github/scripts/alioss/run-int.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+script_dir="$( cd "$(dirname "${0}")" && pwd )"
+repo_root="$(cd "${script_dir}/../../.." && pwd)"
+
+: "${access_key_id:?}"
+: "${access_key_secret:?}"
+: "${test_name:=general}"
+: "${region:=cn-hangzhou}"
+
+export ACCESS_KEY_ID="${access_key_id}"
+export ACCESS_KEY_SECRET="${secret_access_key}"
+
+pushd "${script_dir}" > /dev/null
+    source utils.sh
+    bucket_name="$(read_bucket_name_from_file "${test_name}")"
+    : "${bucket_name:?}"
+    export BUCKET_NAME="${bucket_name}"
+popd > /dev/null
+
+export ENDPOINT="https://${BUCKET_NAME}.${region}.aliyuncs.com"
+
+pushd "${repo_root}" > /dev/null
+  echo -e "\n running tests with $(go version)..."
+  ginkgo -r alioss/integration/
+popd > /dev/null
+
