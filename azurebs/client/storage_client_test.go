92203dc0461d73d5b41bb087a683c350da57b61b
Jochen Ehret <jochen.ehret@sap.com>
2026-09-16 12:55:03 +0200

    Test to detect drift in Azure SDK http settings
    * test fails if there are changes in runtime/transport_default_http_client.go

--- azurebs/client/storage_client_test.go
+++ azurebs/client/storage_client_test.go
@@ -1,6 +1,13 @@
 package client_test
 
 import (
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
@@ -47,5 +54,39 @@ var _ = Describe("NewStorageClient", func() {
 
 		_, err := client.NewStorageClient(cfg)
 		Expect(err).To(MatchError(ContainSubstring("must be greater than 0")))
+	})
+})
+
+// Pinned against github.com/Azure/azure-sdk-for-go/sdk/azcore@v1.23.1.
+// If this test fails: diff runtime/transport_default_http_client.go at the new
+// version, update buildClientOptions in storage_client.go to reflect any
+// changes, then update pinnedHash (and pinnedVersion for clarity) below.
+var _ = Describe("azcore transport defaults drift detection", func() {
+	FIt("transport_default_http_client.go has not changed since it was last reviewed", func() {
+		const pinnedVersion = "v1.23.1"
+		const pinnedHash = "e60db6ff9e71a7f778503c3628c8cda73c5fd555436b470696a0c491fb6db20d"
+
+		out, err := exec.Command("go", "list", "-m", "-json", "github.com/Azure/azure-sdk-for-go/sdk/azcore").Output()
+		Expect(err).NotTo(HaveOccurred(), "go list failed: %v", err)
+
+		var modInfo struct {
+			Version string
+			Dir     string
+		}
+		Expect(json.Unmarshal(out, &modInfo)).To(Succeed())
+		Expect(modInfo.Dir).NotTo(BeEmpty(), "azcore module directory not found")
+
+		transportFile := filepath.Join(modInfo.Dir, "runtime", "transport_default_http_client.go")
+		content, err := os.ReadFile(transportFile)
+		Expect(err).NotTo(HaveOccurred(),
+			"could not read azcore transport file at %s", transportFile)
+
+		actualHash := fmt.Sprintf("%x", sha256.Sum256(content))
+		Expect(actualHash).To(Equal(pinnedHash),
+			"azcore transport defaults changed in %s (last reviewed at %s).\n"+
+				"Diff runtime/transport_default_http_client.go, update buildClientOptions "+
+				"in storage_client.go if needed, then update pinnedVersion and pinnedHash in this test.",
+			modInfo.Version, pinnedVersion,
+		)
 	})
 })
